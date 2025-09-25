07c6a0afb83facac87b6f83070ccbc4968a604d0
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-25 17:46:29 +0300

    Added DenecozAplication

---
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Application/DenecozAplication.kt
@@ -0,0 +1,9 @@
+package com.anlarsinsoftware.denecoz.Application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DenecozApplication : Application() {
+
+}

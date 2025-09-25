4b353c8b70a5527cdba8cd03582658ad0631e48f
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-25 17:46:44 +0300

    Created AppModule.kt

---
+++ app/src/main/java/com/anlarsinsoftware/denecoz/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.anlarsinsoftware.denecoz.di
+
+import com.anlarsinsoftware.denecoz.Repository.AuthRepository
+import com.anlarsinsoftware.denecoz.data.repository.AuthRepositoryImpl
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(impl: AuthRepositoryImpl): AuthRepository = impl
+}

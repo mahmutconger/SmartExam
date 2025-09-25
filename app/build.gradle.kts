5f7aa1dbce86ed29b3cb9f00afc20850d6940849
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-25 17:48:09 +0300

    added libs (hilt,firebaseCrash)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.firebase.crashlytics)
+    id("com.google.gms.google-services")
     id("kotlin-kapt")
-   // id("com.google.dagger.hilt.android.plugin")
+    alias(libs.plugins.hilt)
 }
 
 android {

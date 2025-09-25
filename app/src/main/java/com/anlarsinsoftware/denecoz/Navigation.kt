6e29301e054c6961c68dc8aafe1e03779457e4e5
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-25 13:37:32 +0300

    Created

---
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Navigation.kt
@@ -0,0 +1,35 @@
+package com.anlarsinsoftware.denecoz
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.anlarsinsoftware.denecoz.View.EnteranceScreens.LoginScreen
+import com.anlarsinsoftware.denecoz.View.EnteranceScreens.RegisterScreen
+import com.anlarsinsoftware.denecoz.View.HomeScreen.HomeScreen
+
+sealed class Screen(val route: String) {
+    object LoginScreen : Screen("login_screen")
+    object RegisterScreen : Screen("register_screen")
+    object HomeScreen : Screen("home_screen")
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.LoginScreen.route
+    ) {
+        composable(route = Screen.LoginScreen.route) {
+            LoginScreen()
+        }
+        composable(route = Screen.RegisterScreen.route) {
+            RegisterScreen()
+        }
+        composable(route = Screen.HomeScreen.route) {
+            HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/anlarsinsoftware/denecoz/View/EnteranceScreens/LoginScreen.kt
@@ -0,0 +1,19 @@
+package com.anlarsinsoftware.denecoz.View.EnteranceScreens
+
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Giriş Ekranı")
+    }
+}

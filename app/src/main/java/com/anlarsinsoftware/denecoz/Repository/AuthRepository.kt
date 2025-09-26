d03192a5c81f284332fb7fade5592f189d579b13
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-26 17:14:04 +0300

    added new param userRole

--- app/src/main/java/com/anlarsinsoftware/denecoz/Repository/AuthRepository.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Repository/AuthRepository.kt
@@ -1,6 +1,9 @@
 package com.anlarsinsoftware.denecoz.Repository
 
+import com.anlarsinsoftware.denecoz.Model.UserRole
+import com.google.firebase.auth.FirebaseUser
+
 interface AuthRepository {
-    suspend fun registerUser(email: String, pass: String): Result<Unit>
-    suspend fun loginUser(email: String, pass: String): Result<Unit>
+    suspend fun registerUser(email: String, pass: String, role: UserRole): FirebaseUser
+    suspend fun loginUser(email: String, pass: String,role: UserRole): Result<Unit>
 }

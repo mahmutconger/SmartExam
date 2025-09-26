3270591bb0b60df2016cbb8b08312582b63f2342
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-26 17:23:08 +0300

    Added Role params

--- app/src/main/java/com/anlarsinsoftware/denecoz/ViewModel/EnteranceViewModels/LoginViewModel.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/ViewModel/EnteranceViewModels/LoginViewModel.kt
@@ -3,6 +3,7 @@ package com.anlarsinsoftware.denecoz.ViewModel.EnteranceViewModels
 import com.anlarsinsoftware.denecoz.Repository.AuthRepository
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.anlarsinsoftware.denecoz.Model.UserRole
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,9 +18,9 @@ class LoginViewModel @Inject constructor(
     private val _loginState = MutableStateFlow<Result<Unit>?>(null)
     val loginState = _loginState.asStateFlow()
 
-    fun loginUser(email: String, pass: String) {
+    fun loginUser(email: String, password: String, role: UserRole) {
         viewModelScope.launch {
-            val result = repository.loginUser(email, pass)
+            val result = repository.loginUser(email, password, role)
             _loginState.value = result
         }
     }
--- app/src/main/java/com/anlarsinsoftware/denecoz/ViewModel/EnteranceViewModels/RegisterViewModel.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/ViewModel/EnteranceViewModels/RegisterViewModel.kt
@@ -3,10 +3,12 @@ package com.anlarsinsoftware.denecoz.ViewModel.EnteranceViewModels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.anlarsinsoftware.denecoz.Model.UserRole
 import com.anlarsinsoftware.denecoz.Repository.AuthRepository
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,13 +17,17 @@ class RegisterViewModel @Inject constructor(
     private val repository: AuthRepository
 ) : ViewModel() {
 
-    private val _registerState = MutableStateFlow<Result<Unit>?>(null)
-    val registerState = _registerState.asStateFlow()
+    private val _registerState = MutableStateFlow<Result<FirebaseUser>?>(null)
+    val registerState: StateFlow<Result<FirebaseUser>?> = _registerState
 
-    fun registerUser(email: String, pass: String) {
+    fun registerUser(email: String, password: String, role: UserRole) {
         viewModelScope.launch {
-            val result = repository.registerUser(email, pass)
-            _registerState.value = result
+            try {
+                val firebaseUser = repository.registerUser(email, password, role)
+                _registerState.value = Result.success(firebaseUser)
+            } catch (e: Exception) {
+                _registerState.value = Result.failure(e)
+            }
         }
     }
 }

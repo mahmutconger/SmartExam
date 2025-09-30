fb358f72008856e0f0e0402d1195c94a96787709
Mahmut Can Conger <mahmutconger@gmail.com>
2025-09-30 18:59:15 +0300

    new implemntation (Icons)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,11 +75,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    //Icons
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     // Activity-Compose
-    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.activity:activity-compose:1.11.0")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.5")
 
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")

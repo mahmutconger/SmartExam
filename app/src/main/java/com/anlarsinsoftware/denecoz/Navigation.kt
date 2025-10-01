9f60f2e84e2744edd4f3157c356e05ed880ff759
Mahmut Can Conger <mahmutconger@gmail.com>
2025-10-01 19:22:59 +0300

    Added New Navigations

--- app/src/main/java/com/anlarsinsoftware/denecoz/Navigation.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Navigation.kt
@@ -1,27 +1,42 @@
 package com.anlarsinsoftware.denecoz
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.anlarsinsoftware.denecoz.Model.State.EditorMode
 import com.anlarsinsoftware.denecoz.View.EnteranceScreens.LoginScreen
 import com.anlarsinsoftware.denecoz.View.EnteranceScreens.RegisterScreen
 import com.anlarsinsoftware.denecoz.View.HomeScreen.HomeScreen
 import com.anlarsinsoftware.denecoz.View.PublisherView.AnswerKeyEditorScreen
 import com.anlarsinsoftware.denecoz.View.PublisherView.AnswerKeyScreen
 import com.anlarsinsoftware.denecoz.View.PublisherView.GeneralInfoScreen
+import com.anlarsinsoftware.denecoz.View.PublisherView.PreviewScreen
 import com.anlarsinsoftware.denecoz.View.PublisherView.PubHomeScreen
 
 sealed class Screen(val route: String) {
+    // Student
     object LoginScreen : Screen("login_screen")
     object RegisterScreen : Screen("register_screen")
     object HomeScreen : Screen("home_screen")
+    // Publisher
     object PubHomeScreen : Screen("pub_home_screen")
     object GeneralInfoScreen : Screen("general_info_screen")
-    object AnswerKeyScreen : Screen("answer_key_screen")
-    object AnswerKeyEditorScreen : Screen("answer_key_editor_screen")
 
+    object AnswerKeyScreen : Screen("answer_key_screen/{examId}") {
+        fun createRoute(examId: String) = "answer_key_screen/$examId"
+    }
+    object AnswerKeyEditorScreen : Screen("answer_key_editor_screen/{examId}/{mode}") {
+        fun createRoute(examId: String, mode: EditorMode) = "answer_key_editor_screen/$examId/${mode.name}"
+    }
+    object PreviewScreen : Screen("preview_screen/{examId}") {
+        fun createRoute(examId: String) = "preview_screen/$examId"
+    }
 }
+
+
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
@@ -30,27 +45,56 @@ fun AppNavigation() {
         navController = navController,
         startDestination = Screen.LoginScreen.route
     ) {
+        // Enterance
         composable(route = Screen.LoginScreen.route) {
             LoginScreen(navController = navController)
         }
         composable(route = Screen.RegisterScreen.route) {
             RegisterScreen(navController = navController)
         }
-        //Home Screen
+
+        // Student_Home
         composable(route = Screen.HomeScreen.route) {
             HomeScreen(navController = navController)
         }
-        composable(route = Screen.HomeScreen.route) {
-            PubHomeScreen(navController=navController)
+
+        // Publisher
+
+        // Home
+        composable(route = Screen.PubHomeScreen.route) {
+            PubHomeScreen(navController = navController)
         }
+
+        // General
         composable(route = Screen.GeneralInfoScreen.route) {
-            GeneralInfoScreen(navController=navController)
+            GeneralInfoScreen(navController = navController)
         }
-        composable(route = Screen.AnswerKeyScreen.route) {
-            AnswerKeyScreen(navController=navController)
+
+        // AnswerKey
+        composable(
+            route = Screen.AnswerKeyScreen.route,
+            arguments = listOf(navArgument("examId") { type = NavType.StringType })
+        ) {
+            AnswerKeyScreen(navController = navController)
         }
-        composable(route = Screen.AnswerKeyEditorScreen.route) {
-            AnswerKeyEditorScreen (navController=navController)
+
+        // ManuelAnswerKey
+        composable(
+            route = Screen.AnswerKeyEditorScreen.route,
+            arguments = listOf(
+                navArgument("examId") { type = NavType.StringType },
+                navArgument("mode") { type = NavType.StringType }
+            )
+        ) {
+            AnswerKeyEditorScreen(navController = navController)
+        }
+
+        // Preview
+        composable(
+            route = Screen.PreviewScreen.route,
+            arguments = listOf(navArgument("examId") { type = NavType.StringType })
+        ) {
+            PreviewScreen(navController = navController)
         }
     }
 }

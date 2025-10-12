926abf91a9484c0a6b2480ae74002be561b9fce5
Mahmut Can Conger <mahmutconger@gmail.com>
2025-10-12 15:20:10 +0300

    deleted param(bookletType) and dataclass (OnBookletTypeSelected)

--- app/src/main/java/com/anlarsinsoftware/denecoz/Model/State/GeneralInfoStateEvent.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Model/State/GeneralInfoStateEvent.kt
@@ -1,10 +1,10 @@
 package com.anlarsinsoftware.denecoz.Model.State
 
 import android.net.Uri
+
 data class GeneralInfoUiState(
     val examName: String = "",
     val publicationDate: String = "",
-    val bookletType: String = "",
     val examType: String = "",
     val coverImageUri: Uri? = null,
     val isLoading: Boolean = false,
@@ -15,7 +15,6 @@ data class GeneralInfoUiState(
 sealed class GeneralInfoEvent {
     data class OnExamNameChanged(val name: String) : GeneralInfoEvent()
     data class OnPublicationDateSelected(val date: String) : GeneralInfoEvent()
-    data class OnBookletTypeSelected(val type: String) : GeneralInfoEvent()
     data class OnExamTypeSelected(val type: String) : GeneralInfoEvent()
     data class OnCoverImageSelected(val uri: Uri?) : GeneralInfoEvent()
     object OnContinueClicked : GeneralInfoEvent()

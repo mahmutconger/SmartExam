37d638017500cccc138da55f417ba0c5faa86756
Mahmut Can Conger <mahmutconger@gmail.com>
2025-10-13 12:50:42 +0300

    created PublishedExamSummary.kt

---
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Model/PublishedExamSummary.kt
@@ -0,0 +1,9 @@
+package com.anlarsinsoftware.denecoz.Model
+
+data class PublishedExamSummary(
+    val id: String = "",
+    val name: String = "",
+    val coverImageUrl: String? = null,
+    val publisherName: String = "",
+    val examType: String = ""
+)

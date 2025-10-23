8ecfa644c28b4e9253efe339c125960e90709d29
Mahmut Can Conger <mahmutconger@gmail.com>
2025-10-23 22:53:35 +0300

    Updated params

--- app/src/main/java/com/anlarsinsoftware/denecoz/Model/Student/HistoricalTopicPerformance.kt
+++ app/src/main/java/com/anlarsinsoftware/denecoz/Model/Student/HistoricalTopicPerformance.kt
@@ -1,6 +1,9 @@
 package com.anlarsinsoftware.denecoz.Model.Student
 
 data class HistoricalTopicPerformance(
+    val studentId: String = "",
+    val topicId: String = "",
+    val topicName: String = "",
     val totalCorrect: Int = 0,
     val totalIncorrect: Int = 0,
     val totalEmpty: Int = 0
@@ -10,4 +13,7 @@ data class HistoricalTopicPerformance(
 
     val net: Double
         get() = totalCorrect - (totalIncorrect / 4.0)
+
+    val successRate: Float
+        get() = if (totalQuestions > 0) totalCorrect.toFloat() / totalQuestions else 0f
 }
